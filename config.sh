e81d4358997164523e6aa6d134542809583c6ffb
DavedSprdUser <117520404+DavedSprdUser@users.noreply.github.com>
2023-07-28 21:49:29 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,13 +3,13 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export BRANCH="twrp-5.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="scx35_sp7731gea_taichi" # codename used in device tree
+export DT_LINK="https://github.com/DavedSprdUser/android_device_lenovo_scx35_sp7731gea_taichi" # device tree link
+export DT_BRANCH="A1000m-5.1" # device tree branch
+export VENDOR="lenovo" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
